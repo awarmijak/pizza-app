89adac6de5c250db1005b6146bdc00c643631888
Sharin6 <agaberg@op.pl>
2020-12-16 21:39:50 +0100

    Add config class

--- src/main/java/pl/javasolution/pizzeria/App.java
+++ src/main/java/pl/javasolution/pizzeria/App.java
@@ -1,7 +1,15 @@
 package pl.javasolution.pizzeria;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import pl.javasolution.api.IOrder;
+import pl.javasolution.config.Config;
+import pl.javasolution.implementation.Order;
+
 public class App {
     public static void main(String[] args) {
 
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        IOrder order = context.getBean(Order.class);
+        order.printOrder();
     }
 }
